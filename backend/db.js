89d86ea6a31e652743e8289edb1f7196deee837b
Hariom <82692072+DevHarry066@users.noreply.github.com>
2024-01-21 08:38:37 -0800

    updated the console

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'YOUR_MONGO_URL';
+const MONGO_URI = '';
 
 const connectDB = async () => {
     const conn = mongoose.connect(MONGO_URI, {
@@ -9,7 +9,7 @@ const connectDB = async () => {
         // useFindAndModify: false,
         // useUnifiedTopology: true
     });
-    console.log(`MongoDB database connected: ${(await conn).connection.host}`.cyan.underline.bold);
+    console.log(`MongoDB database connected with PayTM collection: ${(await conn).connection.host}`.cyan.underline.bold);
 }
 
 
